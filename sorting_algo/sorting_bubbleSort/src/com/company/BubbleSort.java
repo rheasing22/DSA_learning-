686218926b0f69cb45ca3fbd8d9dc1b7016a17e7
Rhea <rheasing22@gmail.com>
2021-04-22 15:05:15 +0400

    added to bubble sort

--- sorting_algo/sorting_bubbleSort/src/com/company/BubbleSort.java
+++ sorting_algo/sorting_bubbleSort/src/com/company/BubbleSort.java
@@ -20,6 +20,33 @@ public class BubbleSort {
 
     }
 
+    // An optimized version of Bubble Sort
+    static void OptimizedBubbleSort(int arr[], int n)
+    {
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // IF no two elements were
+            // swapped by inner loop, then break
+            if (swapped == false)
+                break;
+        }
+    }
+
     /* Prints the array */
     void printArray(int arr[])
     {
